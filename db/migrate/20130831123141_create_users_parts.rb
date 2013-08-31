fcc3d384368f70b3ea6ed25d87b4491ee6d7fd0e
Luisa <Emmi.Beck@gmail.com>
2013-08-31 15:44:31 -0700

    updated deleted files

--- db/migrate/20130831123141_create_users_parts.rb
+++
@@ -1,8 +0,0 @@
-class CreateUsersParts < ActiveRecord::Migration
-  def change
-    create_table :users_parts, id: false do |t|
-      t.integer :user_id
-      t.integer :part_id
-    end
-  end
-end
