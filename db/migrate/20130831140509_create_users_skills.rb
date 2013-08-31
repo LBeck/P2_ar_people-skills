fdc596ffccf40af57cf6d5e7795ef3cde742e7f5
Luisa <Emmi.Beck@gmail.com>
2013-08-31 15:20:53 -0700

    migration file updated

---
+++ db/migrate/20130831140509_create_users_skills.rb
@@ -0,0 +1,8 @@
+class CreateUsersSkills < ActiveRecord::Migration
+  def change
+    create_table :users_skills, id: false do |t|
+      t.integer :user_id
+      t.integer :skill_id
+  end
+end
+end
