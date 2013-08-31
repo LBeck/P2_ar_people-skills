c78d5265b9a5b8c817acf1ca1c2611aa44a9bde3
Luisa <Emmi.Beck@gmail.com>
2013-08-31 12:10:57 -0700

    updated migration files

--- db/migrate/20130501155942_create_skills.rb
+++ db/migrate/20130501155942_create_skills.rb
@@ -1,9 +1,9 @@
 class CreateSkills < ActiveRecord::Migration
   def change
     create_table :skills do |t|
-      t.belongs_to :user
       t.string :name
       t.string :context
+      t.integer :user_id
 
       t.timestamps
     end
