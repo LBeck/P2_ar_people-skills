a329f39f5ed9a4f5546ff8ef823c8b3831b18ad1
Luisa <Emmi.Beck@gmail.com>
2013-08-31 15:43:45 -0700

    migration table update

--- db/migrate/20130831140509_create_users_skills.rb
+++ db/migrate/20130831140509_create_users_skills.rb
@@ -2,7 +2,7 @@ class CreateUsersSkills < ActiveRecord::Migration
   def change
     create_table :users_skills, id: false do |t|
       t.belongs_to :user_id
-      t.integer :skill_id
+      t.belongs_to :skill_id
   end
 end
 end
