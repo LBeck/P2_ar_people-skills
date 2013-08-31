4516c765d871b2ff98ae72edc00935e7a8921ee6
Luisa <Emmi.Beck@gmail.com>
2013-08-31 15:40:49 -0700

    modified join table

--- db/migrate/20130831140509_create_users_skills.rb
+++ db/migrate/20130831140509_create_users_skills.rb
@@ -1,7 +1,7 @@
 class CreateUsersSkills < ActiveRecord::Migration
   def change
     create_table :users_skills, id: false do |t|
-      t.integer :user_id
+      t.belongs_to :user_id
       t.integer :skill_id
   end
 end
