02f5c130b779a805390372f2c922587e350eb099
LBeck <Emmi.Beck@gmail.com>
2013-08-31 18:58:14 -0700

    Delete 20130831140509_create_users_skills.rb

--- db/migrate/20130831140509_create_users_skills.rb
+++
@@ -1,8 +0,0 @@
-class CreateUsersSkills < ActiveRecord::Migration
-  def change
-    create_table :users_skills, id: false do |t|
-      t.belongs_to :user_id
-      t.belongs_to :skill_id
-  end
-end
-end
