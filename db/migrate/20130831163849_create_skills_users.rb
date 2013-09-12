548d27798d13d4d485315d623de91606029b22b8
Luisa <Emmi.Beck@gmail.com>
2013-09-12 09:52:46 -0700

    deleted unnecessary files

--- db/migrate/20130831163849_create_skills_users.rb
+++
@@ -1,8 +0,0 @@
-class CreateSkillsUsers < ActiveRecord::Migration
-  def change
-    create_table :skills_users, id: false do |t|
-      t.belongs_to :skill_id
-      t.belongs_to :user_id
-    end
-  end
-end
